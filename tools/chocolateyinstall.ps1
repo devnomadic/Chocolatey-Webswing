39f3869334a4691999c4f9e656b7b73afc374677
Drew Kennedy <drewkenn@gmail.com>
2019-03-15 00:08:48 +1100

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://bitbucket.org/meszarv/webswing/downloads/webswing-2.5.10.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "${env:ProgramFiles(x86)}\Webswing"
+  url           = $url
+  softwareName  = 'Webswing*'
+  checksum      = 'F32ABBBE36AFAD2D92879E5812DC55A5515BEFF5920BA620BA98C7268E3C5E4D'
+  checksumType  = 'sha256'
+  silentArgs   = ''
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'ChocoWebswing*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  ZipFileName   = 'webswing-2.5.10.zip'
+}
+
+$uninstalled = $false
+
+function Uninstall-ChocolateyZipPackageFix {
+  param(
+    [parameter(Mandatory=$true, Position=0)][string] $packageName,
+    [parameter(Mandatory=$true, Position=1)][string] $zipFileName,
+    [parameter(ValueFromRemainingArguments = $true)][Object[]] $ignoredArguments
+  )
+
+  Write-FunctionCallLogMessage -Invocation $MyInvocation -Parameters $PSBoundParameters
+
+  $packagelibPath=$env:chocolateyPackageFolder
+  $zipContentFile=(join-path $packagelibPath $zipFileName) + "Install.txt"
+
+  # The Zip Content File may have previously existed under a different
+  # name.  If *Install.txt doesn't exist, check for the old name
+  if(-Not (Test-Path -Path $zipContentFile)) {
+    $zipContentFile=(Join-Path $packagelibPath -ChildPath $zipFileName) + ".txt"
+  }
+
+  if ((Test-Path -path $zipContentFile)) {
+    $zipContentFile
+    $zipContents=get-content $zipContentFile
+    foreach ($fileInZip in $zipContents) {
+      if ($fileInZip -ne $null -and $fileInZip.Trim() -ne '') {
+        Remove-Item -Path "$fileInZip" -ErrorAction SilentlyContinue -Recurse -Force
+      }
+    }
+  }
+
+}
+
+Uninstall-ChocolateyZipPackageFix @packageArgs
